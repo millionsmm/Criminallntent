5aac8597094f4c352d0a316e788e2f1ef5a05034
millionsmm <2415122956@qq.com>
2017-06-29 21:47:41 +0800

    自主添加，层级导航

--- app/src/main/java/activitytest/example/com/criminallntent/SingleFragmentActivity.java
+++ app/src/main/java/activitytest/example/com/criminallntent/SingleFragmentActivity.java
@@ -5,12 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Administrator on 2017/6/26 0026.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     @Override
