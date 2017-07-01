5451423a79cf63040a1d318bdf3c821f43cf4bbb
millionsmm <2415122956@qq.com>
2017-07-01 22:43:15 +0800

    出现了CrimeFrag无法显示的bug

--- app/src/main/java/activitytest/example/com/criminallntent/CrimeFragment.java
+++ app/src/main/java/activitytest/example/com/criminallntent/CrimeFragment.java
@@ -32,6 +32,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args=new Bundle();
@@ -97,6 +98,19 @@ public class CrimeFragment extends Fragment {
                 mCrime.setSolved(isChecked);
             }
         });
+        /**
+        mReportButton=(Button)view.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT,getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.crime_report_subject));
+                startActivity(i);
+            }
+        });
+         */
         return view;
     }
 
