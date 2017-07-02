0489c654bae0fd5eeaa8274562125a5a5fb55028
millionsmm <2415122956@qq.com>
2017-07-02 23:22:55 +0800

    为新增按钮增添绑定

--- app/src/main/java/activitytest/example/com/criminallntent/CrimeFragment.java
+++ app/src/main/java/activitytest/example/com/criminallntent/CrimeFragment.java
@@ -20,6 +20,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Date;
 import java.util.UUID;
@@ -39,6 +41,8 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args=new Bundle();
@@ -135,6 +139,9 @@ public class CrimeFragment extends Fragment {
         if (packageManager.resolveActivity(pickContact,PackageManager.MATCH_DEFAULT_ONLY)==null){
             mSuspectButton.setEnabled(false);
         }
+
+        mPhotoButton=(ImageButton) view.findViewById(R.id.crime_camera);
+        mPhotoView=(ImageView)view.findViewById(R.id.crime_photo);
 
         return view;
     }
